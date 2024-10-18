46d73af8da88b8d49b692fe172f635a2b687c392
PokemonOwner <160018170+PokemonOwner@users.noreply.github.com>
2024-10-18 14:30:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
 
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
@@ -13,7 +11,7 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            plugins=dict(root="TechVJ"),
+            plugins=dict(root="Hell"),
             workers=50,
             sleep_threshold=10
         )
@@ -22,13 +20,9 @@ class Bot(Client):
     async def start(self):
             
         await super().start()
-        print('Bot Started Powered By @VJ_Botz')
+        print('Bot Started Powered By @King_Of_Hell_Botz')
 
     async def stop(self, *args):
 
         await super().stop()
         print('Bot Stopped Bye')
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
